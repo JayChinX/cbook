4242d40de3708be96c77b147d2257d88179ec320
秦小杰 <qinxiaojie@58.com>
2023-11-15 19:27:14 +0800

    videos scripts

---
+++ ffmpeg/videos/README.md
@@ -0,0 +1,3 @@
+`./mp4dump --format text input.mp4 > dump.txt`
+`./mp4info --verbose input.mp4`
+`./moovchange.sh input.mp4`
---
+++ ffmpeg/videos/moovchange.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# dat for /R %%v IN (*.mp4) do ( ffmpeg -i %%v -vcodec h264  "path\%%~nv.mp4")
+if [ $# == 0 ]; then
+    for item in *.mp4; do
+    ffmpeg -i ${item} -movflags faststart -c copy  ${item%%.*}_mv.mp4
+    # echo ${item%%.*}.mp4
+    done
+else
+    for item in $@; do
+        ffmpeg -i ${item} -movflags faststart -c copy  ${item%%.*}_mv.mp4
+    done
+    
+fi
+
+
