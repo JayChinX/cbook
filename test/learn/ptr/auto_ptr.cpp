f2f1c424e805b493350c8d37ec06666513a2d345
qin <18339989537@163.com>
2023-12-06 23:19:07 +0800

    auto ptr

--- test/learn/ptr/auto_ptr.cpp
+++ test/learn/ptr/auto_ptr.cpp
@@ -7,8 +7,14 @@ int main(int argc, char **argv)
     std::shared_ptr<int> c_ptr = std::make_shared<int>(1);
     std::cout << "c: " << *c_ptr.get() << std::endl;
 
+    std::shared_ptr<int> e_ptr(new int(10));
+    std::shared_ptr<int> f_ptr = e_ptr;
+    std::cout << "e: " << *e_ptr.get() << std::endl;
+    std::cout << "f: " << *f_ptr.get() << std::endl;
+
     int a = 123;
-    // std::unique_ptr<int> u_ptr = a;
+    std::unique_ptr<int> u_ptr(new int(13));
+    std::cout << "a: " << *u_ptr.get() << std::endl;
 
     system("pause");
     return 0;
