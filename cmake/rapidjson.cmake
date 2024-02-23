6d9828c28ed136309e35bef4ec4073eeec1b808e
秦小杰 <qinxiaojie@58.com>
2024-02-23 10:44:27 +0800

    cmake presets

--- cmake/rapidjson.cmake
+++ cmake/rapidjson.cmake
@@ -1,11 +1,11 @@
-include(FetchContent)
-FetchContent_Declare(
-  rapidjson
-# URL https://github.com/google/googletest/archive/c875c4e2249ec124c24f72141b3780c22256fd44.zip
-  GIT_REPOSITORY https://github.com/Tencent/rapidjson.git
-  GIT_TAG v1.1.0
-# URL_HASH  SHA256=${FILE_HASH}
-)
-# FetchContent_MakeAvailable(googletest) 是将Google Test框架作为外部项目导入到CMake的当前项目中。这个命令会下载、构建并将Google Test的源代码和构建结果添加到当前项目中，使得可以在项目中直接使用Google Test的功能。
-FetchContent_MakeAvailable(rapidjson)
+# include(FetchContent)
+# FetchContent_Declare(
+#   rapidjson
+# # URL https://github.com/google/googletest/archive/c875c4e2249ec124c24f72141b3780c22256fd44.zip
+#   GIT_REPOSITORY https://github.com/Tencent/rapidjson.git
+#   GIT_TAG v1.1.0
+# # URL_HASH  SHA256=${FILE_HASH}
+# )
+# # FetchContent_MakeAvailable(googletest) 是将Google Test框架作为外部项目导入到CMake的当前项目中。这个命令会下载、构建并将Google Test的源代码和构建结果添加到当前项目中，使得可以在项目中直接使用Google Test的功能。
+# FetchContent_MakeAvailable(rapidjson)
 
